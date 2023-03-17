fcb8703939dadba5f317540fdf3fab9274f94e25
deshaxii <mustafa.fathi7@gmail.com>
2023-03-17 19:31:19 +0200

    fix some issue

--- pages/api/blog.js
+++ pages/api/blog.js
@@ -1,9 +1,14 @@
 import nc from "next-connect";
 import { create, getMultiple } from "../../controllers/blogController";
+import cors from "cors";
 
 const handler = nc();
 
-console.log(handler);
+app.use(
+  cors({
+    origin: "https://climate-nextjs-mysql.vercel.app/",
+  })
+);
 
 handler.get(getMultiple);
 handler.post(create);
