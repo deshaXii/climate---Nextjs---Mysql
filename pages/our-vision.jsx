f852f645921dca985ccd035c63b22901281fef4d
Mustafa Fathi <mustafa.fathi7@gmail.com>
2023-04-03 03:58:12 +0200

    add padding to our vision section

--- pages/our-vision.jsx
+++ pages/our-vision.jsx
@@ -18,31 +18,34 @@ const OurVision = ({ info }) => {
             image="/images/Our-teaam-title-img.jpg"
             marked="True experince"
           />
-          <div className="container">
-            <div className="row">
-              <div className="col-md-6">
-                <div className="vision--card">
-                  <div className="vc-p">
-                    <h3>As President and CEO of CLIMATE</h3>
-                    <p>
-                      it's my mission to make decisions aligned with our vision,
-                      and to connect and empower our staff to deliver on our
-                      four differentiators of providing legendary client
-                      experiences, unmatched employee experiences, to innovate
-                      and differentiate, and to remain private. With more than
-                      15 years in the career, I give this advice to an upcoming
-                      engineer:
-                    </p>
-                    <blockquote>
-                      "While it's important to be an expert in your field,
-                      always maintain a broad perspective and get as much
-                      exposure to other areas as you can. Don't limit yourself.”
-                    </blockquote>
+          <section className="pt80">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="vision--card">
+                    <div className="vc-p">
+                      <h3>As President and CEO of CLIMATE</h3>
+                      <p>
+                        it's my mission to make decisions aligned with our
+                        vision, and to connect and empower our staff to deliver
+                        on our four differentiators of providing legendary
+                        client experiences, unmatched employee experiences, to
+                        innovate and differentiate, and to remain private. With
+                        more than 15 years in the career, I give this advice to
+                        an upcoming engineer:
+                      </p>
+                      <blockquote>
+                        "While it's important to be an expert in your field,
+                        always maintain a broad perspective and get as much
+                        exposure to other areas as you can. Don't limit
+                        yourself.”
+                      </blockquote>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+          </section>
         </div>
       </Default>
     </>
